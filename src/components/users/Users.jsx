00dec6c300162269d238495020601115b708dfd3
felixGlez <felixgonzalezgarcia2010@gmail.com>
2024-03-21 20:22:21 +0100

    Update Users.jsx

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -9,7 +9,7 @@ import {
 } from './styles';
 
 const Users = ({ filteredUsers }) => {
-	// Hook para navegar desde cualquier elemento HTML
+	// Hook para navegar desde cualquier elemento HTML que no sea un <a>.
 	const navigate = useNavigate();
 
 	return (
@@ -24,11 +24,18 @@ const Users = ({ filteredUsers }) => {
 					<StyledStatus $isActive={user.active}>
 						{user.active ? 'Activo' : 'Inactivo'}
 					</StyledStatus>
+
+					{/*
+					Esto lo comento porque semánticamente en HTML está mal hecho, en todo caso sería el link (<a>) el que envuelve al botón
+
 					<button>
 						<Link to={`/${user.userId}`}>Ver Detalles</Link>
-					</button>
-					{/* <button onClick={() => navigate(`/${user.userId}`)}> Ver Detalles
 					</button> */}
+
+					<button onClick={() => navigate(`/${user.userId}`)}>
+						{' '}
+						Ver Detalles
+					</button>
 				</StyledUserCard>
 			))}
 		</StyledUsersContainer>
